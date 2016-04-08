f8b4ff2f829e04354bc8fffed4045b686587e897
MarcProe <marcus@proest.net>
2016-04-08 21:14:13 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -5,11 +5,16 @@ cd librepilot
 git checkout next
 
 make all_sdk_install
+make osg_install
+make osgearth_install
 
-make uavobjects
+make opfw_resource
+make gcs package uavobjects
+
+#make uavobjects
 mkdir makeobjects
 cd makeobjects
 zip -j -5 next.zip ../shared/uavobjectdefinition/*.xml
 #../build/uavobjgenerator/uavobjgenerator -java -v ../shared/uavobjectdefinition ..
-ls -al
+#ls -al
 cd ..
