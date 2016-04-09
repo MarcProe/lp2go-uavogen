24a27d4b1a395f17c83ac944304096302dac23c9
MarcProe <marcus@proest.net>
2016-04-09 22:22:01 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -17,16 +17,16 @@ rm -rf lp2go-uavo
 git clone https://${GH_REF}
 cd lp2go-uavo
 
-#rm ${ZIP}-${COMMIT}.zip 
+rm ${ZIP}_-${COMMIT}.zip 
 echo "#"
-echo ${ZIP}-${COMMIT}.zip
+echo ${ZIP}_${COMMIT}.zip
 echo "#"
-zip -j -5 "${ZIP}-${COMMIT}.zip" ../$2/shared/uavobjectdefinition/*.xml
+zip -j -5 "${ZIP}_${COMMIT}.zip" ../$2/shared/uavobjectdefinition/*.xml
 
 git config --global user.email "marcus@proest.net"
 git config --global user.name "Marc"
 
-git add "${ZIP}-${COMMIT}.zip"
+git add "${ZIP}_${COMMIT}.zip"
 git commit -m "${ZIP} ${COMMIT}"
 
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
