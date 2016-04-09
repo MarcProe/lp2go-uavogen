1e1451e926dff58710df615210fe970f97a75380
MarcProe <marcus@proest.net>
2016-04-09 21:22:36 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -1,8 +1,8 @@
 ﻿#!/usr/bin/env bash
 
-git clone https://bitbucket.org/librepilot/librepilot.git
-cd librepilot
-git checkout next
+git clone $1
+cd $2
+git checkout $3
 
 export COMMIT=`git describe`
 
@@ -12,7 +12,7 @@ git clone https://${GH_REF}
 cd lp2go-uavo
 
 rm next-${COMMIT}.zip 
-zip -j -5 next-${COMMIT}.zip ../librepilot/shared/uavobjectdefinition/*.xml
+zip -j -5 next-${COMMIT}.zip ../$2/shared/uavobjectdefinition/*.xml
 
 git config --global user.email "marcus@proest.net"
 git config --global user.name "Marc"
@@ -21,6 +21,5 @@ git add next-${COMMIT}.zip
 git commit -m "${COMMIT}"
 
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
-
 
 cd ..
