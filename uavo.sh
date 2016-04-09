e6907cef099ee362297b0859cfeaff80b86c3683
MarcProe <marcus@proest.net>
2016-04-09 20:35:49 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -4,11 +4,13 @@ git clone https://bitbucket.org/librepilot/librepilot.git
 cd librepilot
 git checkout next
 
+export COMMIT=`git describe`
 #make uavobjects
 
 mkdir makeobjects
 
-zip -j -5 makeobjects/next-`git describe`.zip shared/uavobjectdefinition/*.xml
+
+zip -j -5 makeobjects/next-${COMMIT}.zip shared/uavobjectdefinition/*.xml
 
 cd makeobjects
 
@@ -16,8 +18,10 @@ git init
 git config --global user.email "marcus@proest.net"
 git config --global user.name "Marc"
 
-git add *.zip
-git commit -m "Yo."
+git add next-${COMMIT}.zip
+git commit -m "${COMMIT}"
+
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
 
 #curl -X POST \--verbose \--header "Content-Type: application/zip" \--data-binary @next-`git describe`.zip \https://api.github.com/gists
 
