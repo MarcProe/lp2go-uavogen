28f47f75be217453af5b0b4a6f1a94602c8fcb84
MarcProe <marcus@proest.net>
2016-04-09 21:49:53 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -1,5 +1,7 @@
 ﻿#!/usr/bin/env bash
 
+export ZIP=${3//\//-}
+
 rm -rf $2
 
 git clone $1
@@ -13,14 +15,14 @@ cd ..
 git clone https://${GH_REF}
 cd lp2go-uavo
 
-rm next-${COMMIT}.zip 
-zip -j -5 next-${COMMIT}.zip ../$2/shared/uavobjectdefinition/*.xml
+rm ${ZIP}-${COMMIT}.zip 
+zip -j -5 ${ZIP}-${COMMIT}.zip ../$2/shared/uavobjectdefinition/*.xml
 
 git config --global user.email "marcus@proest.net"
 git config --global user.name "Marc"
 
-git add next-${COMMIT}.zip
-git commit -m "${COMMIT}"
+git add ${ZIP}-${COMMIT}.zip
+git commit -m "${ZIP} ${COMMIT}"
 
 git push --quiet "https://${GH_TOKEN}@${GH_REF}" master > /dev/null 2>&1
 
