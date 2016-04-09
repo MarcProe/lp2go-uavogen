725e3944088586ae4502f0ca4aa675b767d07cb1
MarcProe <marcus@proest.net>
2016-04-09 19:18:14 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -4,13 +4,15 @@ git clone https://bitbucket.org/librepilot/librepilot.git
 cd librepilot
 git checkout next
 
-make uavobjects
+#make uavobjects
 
 mkdir makeobjects
 cd makeobjects
 zip -j -5 next-`git describe`.zip ../shared/uavobjectdefinition/*.xml
 
-curl -X POST \--data-binary '{"files": {"next-`git describe`.zip": {"content": "Hello, SO"}}}' \https://api.github.com/gists
+curl -X POST \--header "Content-Type: application/zip" \--data-binary @next-`git describe`.zip \https://api.github.com/gists
+
+#curl --verbose --header "Content-Type: application/zip" --data-binary @C:\Junk\test.zip "http://client.xyz.com/submit?username=test@test.com&password=testpassword&job=test"
 
 #../build/uavobjgenerator/uavobjgenerator -java -v ../shared/uavobjectdefinition ..
 ls -al
