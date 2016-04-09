43b14d8216ff5a2079dab681df939523a94300a6
MarcProe <marcus@proest.net>
2016-04-09 19:28:58 +0200

    -

--- uavo.sh
+++ uavo.sh
@@ -10,7 +10,7 @@ mkdir makeobjects
 cd makeobjects
 zip -j -5 next-`git describe`.zip ../shared/uavobjectdefinition/*.xml
 
-curl -X POST \--header "Content-Type: application/zip" \--data-binary @next-`git describe`.zip \https://api.github.com/gists
+curl -X POST \--verbose \--header "Content-Type: application/zip" \--data-binary @next-`git describe`.zip \https://api.github.com/gists
 
 #curl --verbose --header "Content-Type: application/zip" --data-binary @C:\Junk\test.zip "http://client.xyz.com/submit?username=test@test.com&password=testpassword&job=test"
 
